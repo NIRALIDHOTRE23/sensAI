631440dae4f4ef221e162cbb94f74d678f8f83e8
Nirali Dhotre <nirali.dhotre>
2025-04-08 14:32:32 +0530

    Initial commit

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,5 +1,7 @@
 import { inngest } from "@/lib/inngest/client";
 import { serve } from "inngest/next";
+import { generateIndustryInsights } from '@/lib/inngest/functions';
+
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
